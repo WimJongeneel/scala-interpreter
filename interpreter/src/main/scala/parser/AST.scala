db8c0b80373df72d34c77a22fe9a93515f0ea407
WimJongeneel <wim.jongeneel1@gmail.com>
2022-04-03 14:18:43 +0200

    Add show function for AST and expressions

--- interpreter/src/main/scala/parser/AST.scala
+++ interpreter/src/main/scala/parser/AST.scala
@@ -13,3 +13,23 @@ case class Declaration(name: String, expr: Expression) extends AST
 case class ExpressionStatement(expr: Expression) extends AST
 case class PrintExpression(expr: Expression) extends AST
 case class WhileLoop(cond: Expression, body: Expression) extends AST
+
+def show(e: Expression, tabs: String = "\t"): String = {
+    e match {
+        case Literal(n) => n.toString
+        case UnaryOperator(o, e) => s"UnaryOperator(\n$tabs" + o + s"\n$tabs" + show(e, tabs + "\t") + "\n)"
+        case BinaryOperator(l, o, r) => s"BinaryOperator(\n$tabs" + show(l, tabs + "\t") + s"\n$tabs" + o + s"\n$tabs" + show(r, tabs + "\t") + "\n)"
+        case Reference(n) => "Reference("+n+")"
+        case IfThenElse(c, t, f) => s"IfThenElse(\n$tabs" + show(c, tabs + "\t") + s"\n$tabs" + show(t, tabs + "\t") + s"\n$tabs" + show(f, tabs + "\t") + "\n)"
+        case CodeBlock(e) => "CodeBlock(" + e.foldLeft(tabs)((r, e) => r + "\n" + tabs + showAST(e)) + "\n)"
+    }
+}
+
+def showAST(e: AST, tabs: String = "\t"): String = {
+    e match {
+        case Declaration(n, e) => s"Declaration(\n$tabs" + n + s"\n$tabs" + show(e, tabs + "\t") + "\n)"
+        case ExpressionStatement(e) => "Expression(" + show(e, tabs + "\t") + ")"
+        case PrintExpression(e) => "Print(" + show(e, tabs + "\t") + ")"
+        case WhileLoop(c, b) => s"WhileLoop(\n$tabs" + show(c, tabs + "\t") + s",\n$tabs" + show(b, tabs + "\t") + "\n)"
+    }
+}
