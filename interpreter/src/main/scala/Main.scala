01adb0d6ff1cd6d61f308cd0eab0d85169f9ece4
WimJongeneel <wim.jongeneel1@gmail.com>
2022-04-03 09:28:41 +0200

    Fix whitespace stripping in lexer

--- interpreter/src/main/scala/Main.scala
+++ interpreter/src/main/scala/Main.scala
@@ -5,7 +5,7 @@ import runtime._
 import lexer._
 
 @main def main: Unit = 
-  val code = "let x = 2; let y = 3; x + y; print 3 * { let r = 2; r * 2; };"
+  val code = "let x = 2; let y = 3; x + y; print 3 * { let r = if x then 1 + 1 else 1 + 1 + 1; r * 2; };"
   println(lex(code))
   println(parse(ParserState(lex(code))))
   run(parse(ParserState(lex(code))))
